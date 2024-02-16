fb36e2aa5b7e5a40fc92b71b750be74418a20a9b
Amarendhar Reddy Chevula <98495106+amarendhar-reddy@users.noreply.github.com>
2024-02-16 00:58:10 -0600

    Add files via upload

---
+++ BlogDjangoApp/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post,Announcement, Calendar
+
+admin.site.register(Post)
+admin.site.register(Announcement)
+admin.site.register(Calendar)
---
+++ BlogDjangoApp/blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Calendar(models.Model):
+    date_event = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+
+
